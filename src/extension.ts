abdf44e95f492c5992f8abc44b8648e9e0efcf3c
wangrui <laofyx@163.com>
2024-01-24 17:04:34 +0800

    [fixed] 初始实例

--- src/extension.ts
+++ src/extension.ts
@@ -2,19 +2,21 @@ import * as vscode from 'vscode'
 import { File } from './file'
 import { getActiveEditor } from './utils'
 
+const fileInstance: File = File.getInstance()
+
 export async function activate(context: vscode.ExtensionContext) {
   let editor = getActiveEditor()
   if (!editor) return
 
-  File.getInstance().handlerFile(editor.document.fileName)
+  fileInstance.handlerFile(editor.document.fileName)
 
   context.subscriptions.push(
     vscode.commands.registerCommand('simple-logs.clearCache', () => {
-      File.getInstance().clearCache(true)
+      fileInstance.clearCache(true)
     })
   )
 }
 
 export function deactivate() {
-  File.getInstance().dispose()
+  fileInstance.dispose()
 }
